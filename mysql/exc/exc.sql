9f02a031dd4842e4c096c429668efd8909ad80b6
chakyungsuk <cha0054@naver.com>
2022-01-24 11:52:31 +0900

    .

---
+++ mysql/exc/exc.sql
@@ -0,0 +1,25 @@
+use world;
+
+show tables;
+
+-- 주석표시
+
+-- 테이블 생성
+create table member (
+	seq int,
+	name varchar(100),
+    id varchar(100),
+    regdate datetime
+);
+
+create table member2 (
+	seq int,
+	name varchar(100),
+    id varchar(100),
+    regdate datetime
+);
+
+-- 테이블 삭제
+
+
+
