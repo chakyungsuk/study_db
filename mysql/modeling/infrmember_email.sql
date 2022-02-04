70f177538940f2e2117ca59015040b74547eaf6e
chakyungsuk <cha0054@naver.com>
2022-02-04 17:42:35 +0900

    .

--- mysql/modeling/infrmember_email.sql
+++ mysql/modeling/infrmember_email.sql
@@ -2,12 +2,11 @@ CREATE TABLE IF NOT EXISTS `infrMember_email` (
   `seq` INT NOT NULL,
   `default_ny` TINYINT(1) NULL,
   `emailtype_cd` TINYINT(1) NULL,
-  `companyemail` VARCHAR(45) NULL,
-  `homeemail` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
   `infrMember_seq` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`seq`),
   INDEX `fk_infrMember_sns_infrMember1_idx` (`infrMember_seq` ASC) VISIBLE,
-  CONSTRAINT `fk_infrMember_sns_infrMember10`
+  CONSTRAINT `fk_infrMember_sns_infrMember100`
     FOREIGN KEY (`infrMember_seq`)
     REFERENCES `infrMember` (`seq`)
     ON DELETE NO ACTION
@@ -15,15 +14,20 @@ CREATE TABLE IF NOT EXISTS `infrMember_email` (
 ENGINE = InnoDB;
 
 insert into infrmember_email (
-default_ny
+seq
+,default_ny
 ,emailtype_cd
-,companyemail
-,homeemail
-,infrmember_seq
+,email
+,infrMember_seq
 ) values (
-'1'
+1
+,'1'
 ,'1'
 ,'sdkjfkajw@naver.com'
-,'chachacha@naver.com'
 ,'1'
 );
+
+select * from infrmember_email;
+
+
+drop table infrmember_email;
